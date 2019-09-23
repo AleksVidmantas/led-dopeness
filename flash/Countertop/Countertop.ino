130d0705e386919ca1bb74ce4668aa56d44582a0
lessthantrue <nickm@outofthisworld.net>
2019-09-23 01:30:03 -0700

    did some stuff ay

--- flash/Countertop/Countertop.ino
+++ flash/Countertop/Countertop.ino
@@ -2,26 +2,36 @@
 #include "heatPattern.h"
 #include "PerlinOperators.h"
 #include "FillOperator.h"
+#include "Transform.h"
 #include "LedMap.h"
 
-CHSV ledColors[TRIANGLE_LED_COUNT];
-CRGB ledRaw[TRIANGLE_LED_COUNT];
+CHSV ledColors[TRIANGLE_LED_COUNT + SHELF_LED_COUNT + CIELING_LED_COUNT + COUCH_LED_COUNT];
+CRGB ledRaw[TRIANGLE_LED_COUNT + SHELF_LED_COUNT + CIELING_LED_COUNT + COUCH_LED_COUNT];
+
+byte normsquare(byte val){
+  short val_s = (short)val;
+  return (byte)(val * val / 256);
+}
 
 // PerlinHue pHue(20, 5);
 FillOperator fOp(CHSV(36, 255, 255));
 PerlinBrightness pBri(40, 5, 3);
-LayerOperator perlinAll(&fOp, &pBri);
-
+BrightnessTransform bt(&normsquare);
+LayerOperator perlinColor(&fOp, &pBri);
+LayerOperator perlinAll(&perlinColor, &bt);
 
 void setup() {
-  FastLED.addLeds<WS2812B,3,GRB>(ledRaw, TRIANGLE_LED_COUNT);
+  FastLED.addLeds<WS2812B,TRIANGLE_STRIP_PWM,GRB>(ledRaw, TRIANGLE_LED_COUNT);
+  FastLED.addLeds<WS2812B,SHELF_STRIP_PWM, GRB>(ledRaw + TRIANGLE_LED_COUNT, SHELF_LED_COUNT);
+  FastLED.addLeds<WS2812B,CIELING_STRIP_PWM,GRB>(ledRaw + TRIANGLE_LED_COUNT + SHELF_LED_COUNT, CIELING_LED_COUNT);
+  FastLED.addLeds<WS2812B,COUCH_STRIP_PWM,GRB>(ledRaw + TRIANGLE_LED_COUNT + SHELF_LED_COUNT + CIELING_LED_COUNT, COUCH_LED_COUNT);
 //  pp.initialize(convertedColors, numColors);
 //  pp.setResolution(20);
 }
 
 void loop() {
   delay(1);
-  perlinAll.go(ledColors + TRIANGLE_LED_TRIANGLE_START, TRIANGLE_LED_TRIANGLE_LEN);
-  hsv2rgb_spectrum(ledColors, ledRaw, TRIANGLE_LED_COUNT);
+  perlinAll.go(ledColors + TRIANGLE_TRIANGLE_START, TRIANGLE_TRIANGLE_LEN);
+  hsv2rgb_spectrum(ledColors, ledRaw, TRIANGLE_LED_COUNT + SHELF_LED_COUNT + CIELING_LED_COUNT + COUCH_LED_COUNT);
   FastLED.show();
 }
