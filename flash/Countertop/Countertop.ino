b1de90574a63b1261ca5a19c7f0dafeb4e806ef0
lessthantrue <nickm@outofthisworld.net>
2019-09-23 00:19:49 -0700

    we got triangles bois

--- flash/Countertop/Countertop.ino
+++ flash/Countertop/Countertop.ino
@@ -1,37 +1,22 @@
 #include "PerlinPattern.h"
 #include "heatPattern.h"
 #include "PerlinOperators.h"
+#include "FillOperator.h"
 #include "LedMap.h"
 
 CHSV ledColors[TRIANGLE_LED_COUNT];
 CRGB ledRaw[TRIANGLE_LED_COUNT];
-// HeatPattern hp;
-PerlinPattern pp;
 
-PerlinHue pHue(20, 5);
+// PerlinHue pHue(20, 5);
+FillOperator fOp(CHSV(36, 255, 255));
 PerlinBrightness pBri(40, 5, 3);
-LayerOperator perlinAll(&pHue, &pBri);
+LayerOperator perlinAll(&fOp, &pBri);
 
-#define numColors 1
-uint16_t colors[numColors] = {
-  // 0,  //red
-  // 300, //purple
-  // 29, //orange
-  // 85, //green
-  // 170, //mint
-  // 240 //blue
-  51 //GOLD BABY
-};
-uint8_t convertedColors[numColors];
 
 void setup() {
-  for(uint8_t i = 0; i < numColors; i++){
-    //fastled uses 0..255, which is different from everything else
-    convertedColors[i] = colors[i] * 255 / 360;
-  }
   FastLED.addLeds<WS2812B,3,GRB>(ledRaw, TRIANGLE_LED_COUNT);
-  pp.initialize(convertedColors, numColors);
-  pp.setResolution(20);
+//  pp.initialize(convertedColors, numColors);
+//  pp.setResolution(20);
 }
 
 void loop() {
